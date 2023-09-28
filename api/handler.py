7810c6d1bc0d51fa56ef138a0eeb0be1769b3e9e
Marcela De Pretto Amorim <marcelampretto@gmail.com>
2023-09-28 10:10:56 -0300

    Add files

---
+++ api/handler.py
@@ -0,0 +1,44 @@
+import pickle
+import pandas as pd
+
+from rossmann.Rossmann import Rossmann
+from flask import Flask, request, Response
+
+# loading model
+model = pickle.load(open('C:/Users/Usuario/Documents/ds/repos/curso-producao-ds/model/model_rossmann.pkl', 'rb'))
+
+# initialize API
+app = Flask( __name__ )
+@app.route('/rossmann/predict', methods=['POST'])
+
+def rossmann_predict():
+    test_json = request.get_json()
+    
+    if test_json: # there is data
+        if isinstance(test_json, dict): # Unique example
+            test_raw = pd.DataFrame(test_json, index = [0])
+        else: # Multiple examples
+            test_raw = pd.DataFrame(test_json, columns = test_json[0].keys())
+        
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+        
+        # Data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
+        
+        # Feature engineering
+        df2 = pipeline.feature_engineering(df1)
+        
+        # Data preparation
+        df3 = pipeline.data_preparation(df2)
+        
+        # prediction
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+        
+        return df_response
+        
+    else:
+        return Responde('{}', status = 200, mimetype = 'application/json')
+
+if __name__ == '__main__':
+    app.run('localhost') # parâmetro 0.0.0.0 indica que irá rodar na máquina local
